452e42655f3f50ae0c876cca5a7c69a6ea55ff68
Kira Cheung <killercentury@gmail.com>
2017-07-13 16:16:46 +1000

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const theme = {
+  colour: {
+    primary: '#FFCD05',
+    secondary: '#478081',
+    dark: '#004346',
+    light: '#FFFFFF',
+    correct: '#00928A',
+    error: '#BD1903',
+    text: '#000000',
+    neutral: '#F5F3F1',
+    highlight: '#FF6400',
+    border: '#CDCBC3',
+  },
+  tagLine: {
+    highlight: {
+      color: '#FFFFFF',
+      backgroundColor: '#FF6400',
+    },
+  },
+};
+
+export default theme;
